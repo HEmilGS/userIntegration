fdc8f2183173f0cbea261d4626111e906d838688
HEmilGS <hectoremil748@gmail.com>
2024-06-07 23:07:08 -0600

    new header

---
+++ src/shared/NavigationBarNS.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Home } from 'lucide-react';
+import { LogIn } from 'lucide-react';
+import React from "react";
+
+// eslint-disable-next-line react/prop-types
+const NavigationBar = ({ children }) => {
+
+  const handleNavigate = (route) => {
+    window.location.pathname = route;
+  };
+
+
+  return (
+    <div className="flex flex-col h-screen w-full outline relative">
+      <div className="flex shadow-lg z-0 fixed top-0 left-0 right-0">
+        <div
+          onClick={() => handleNavigate("/")}
+          className="flex cursor-pointer m-4 p-2"
+        >
+          <Home size={24} />
+          <p className="mx-2">Dashboard</p>
+        </div>
+        <div
+          onClick={() => handleNavigate("/register")}
+          className="flex cursor-pointer m-4 p-2"
+        >
+          <LogIn size={24} />
+          <p className="mx-2">Register</p>
+        </div>
+
+
+      </div>
+
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child);
+      })}
+    </div>
+  );
+};
+
+export default NavigationBar;
