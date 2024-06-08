2686534a31109b902ea9b5c41bd02a94f9bfa42e
HEmilGS <hectoremil748@gmail.com>
2024-06-07 23:06:51 -0600

    header fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Dashboard from '../views/dashboard/Dashboard';
 import Register from '../views/register/Register';
 import User from '../views/users/User';
 import NavigationBar from './shared/NavigationBar';
+import NavigationBarNS from './shared/NavigationBarNS';
+
 
 function App() {
   return ( 
     <Routes>
       <Route path="/" element = {<NavigationBar><Dashboard /></NavigationBar>} />
-      <Route path="/register" element = {<Register />} />
-      <Route path="/user/:id" element = {<User />} />
+      <Route path="/register" element = {<NavigationBarNS><Register /></NavigationBarNS>} />
+      <Route path="/user/:id" element = {<NavigationBarNS><User /></NavigationBarNS>} />
     </Routes>
   );
 }
--- src/shared/NavigationBar.jsx
+++ src/shared/NavigationBar.jsx
@@ -17,8 +17,8 @@ const NavigationBar = ({ children }) => {
   };
 
   return (
-    <div className="flex flex-col h-screen w-full outline">
-      <div className="flex shadow-lg z-0">
+    <div className="flex flex-col h-screen w-full relative">
+      <div className="flex flex-row  shadow-lg z-0 fixed top-0 left-0 right-0">
         <div
           onClick={() => handleNavigate("/")}
           className="flex cursor-pointer m-4 p-2"
@@ -34,12 +34,16 @@ const NavigationBar = ({ children }) => {
           <p className="mx-2">Register</p>
         </div>
 
+
+      <div className="w-full flex justify-end items-center">
+        <Search size={25}/>
         <input
           className="m-4 p-2 outline outline-black rounded-xl"
           type="text"
-          placeholder="Filtrar por nombre"
+          placeholder="Search users"
           onChange={handleNameChange}
           />
+          </div>
 
       </div>
 
